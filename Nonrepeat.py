05d4720a55aa935901857dee6e5b70171a015034
Patrick <Patrick9211@gmail.com>
2025-01-14 14:23:45 -0700

    Random product and order ids

--- Nonrepeat.py
+++ Nonrepeat.py
@@ -6,8 +6,8 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 import random
 import pandas as pd
+from pyspark.sql import functions as sf
 spark = SparkSession.builder.getOrCreate()
-
 
 def nonRepeat(n):
     numbers = range(0,n)
@@ -19,7 +19,20 @@ def nonRepeat(n):
     print(numList)
     print(numSet)
 
+#Generate a random integer ID 
+#This one allows for repeats to simulate the same customer making multiple orders
+#or the same product being ordered by multiple customers
+#n: number of rows, size: max id number
+def randomIDs(n, size):
+    list = []
+    for i in range(n):
+        #num = int(round(sf.rand(seed=se)*size,0))
+        num = random.randint(0,size)
+        list.append(num)
+    return list
+
 #Generate sequential numbers using pyspark's accumulator
+#n: number of rows
 def accumulate(n):
     list = []
 
@@ -29,10 +42,20 @@ def accumulate(n):
         acc += 1
     return list, acc
 
-list, acc = accumulate(20)
-print(list)
+n = 100
+random.seed(9) #Give random a seed so the results is the same every time
 
-data = {"order_id":list} #Create a dictionary of the data
+list, acc = accumulate(n)
+#print(list)
+
+custIds = randomIDs(n, 20)
+#print(custIds)
+
+prodIds = randomIDs(n, 10)
+#print(prodIds)
+
+data = {"order_id":list, "customer_id":custIds, "product_id":prodIds} #Create a dictionary of the data
+#data = {"order_id":list}
 pdDf = pd.DataFrame(data) #Pass the dictionary to pandas
 
 df = spark.createDataFrame(pdDf) #pass the pandas dataframe to spark
