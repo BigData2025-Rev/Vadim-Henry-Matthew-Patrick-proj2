73847d94dabc70e25540f0a4687cbd5ee77f1b5c
Patrick <Patrick9211@gmail.com>
2025-01-15 09:34:31 -0700

    Random normal indexes
    Adds a function to make random, normally distributed indexes to create a random list of elements picked from the given list.

--- Nonrepeat.py
+++ Nonrepeat.py
@@ -6,7 +6,10 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 import random
 import pandas as pd
+import numpy as np
 from pyspark.sql import functions as sf
+import collections
+
 spark = SparkSession.builder.getOrCreate()
 
 def nonRepeat(n):
@@ -42,24 +45,41 @@ def accumulate(n):
         acc += 1
     return list, acc
 
+#Generate random numbers of a normal distribution and use them as indexes for a list
+#mean: average , dev: standard deviation, n: number of rows, min:lowest random index
+#max: highest random index, list: list of elements to pick from
+def randomNormalIndexes(mean, dev, n, min, max, list):
+    #create the normal distribution and make them into integers
+    normal = np.random.default_rng(seed=9).normal(mean, dev, n).astype(int)
+    indexes = np.clip(normal,a_min=min,a_max=max) #ensure the indexes are not out of range
+    result = [] #Use the random indexes to make a list of random elements from the given list
+    for i in indexes:
+        try:
+            result.append(list[i])
+        except Exception as e: 
+            print("Invalid index")
+            print(e)
+    return result
+
 n = 100
 random.seed(9) #Give random a seed so the results is the same every time
 
 list, acc = accumulate(n)
 #print(list)
-
 custIds = randomIDs(n, 20)
 #print(custIds)
-
 prodIds = randomIDs(n, 10)
 #print(prodIds)
 
-data = {"order_id":list, "customer_id":custIds, "product_id":prodIds} #Create a dictionary of the data
+countries = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+mean = (len(countries))/2-0.5
+countryRandom = randomNormalIndexes(mean, mean/2, n, 0, len(countries)-1,countries)
+#print(countryRandom)
+print(collections.Counter(countryRandom))
+
+data = {"order_id":list, "customer_id":custIds, "product_id":prodIds, "country":countryRandom} #Create a dictionary of the data
 #data = {"order_id":list}
 pdDf = pd.DataFrame(data) #Pass the dictionary to pandas
-
 df = spark.createDataFrame(pdDf) #pass the pandas dataframe to spark
-
 df.show()
-
 spark.stop()
