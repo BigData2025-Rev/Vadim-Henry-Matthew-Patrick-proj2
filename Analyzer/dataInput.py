a801d87c0d71abb9cd5b717c4b68f77b2bbf27f8
Patrick <Patrick9211@gmail.com>
2025-01-21 11:54:55 -0700

    Data cleaning

--- Analyzer/dataInput.py
+++ Analyzer/dataInput.py
@@ -1,11 +1,52 @@
 from pyspark.sql import SparkSession
+import re
+import pandas as pd
 
 spark = SparkSession.builder.appName('S').getOrCreate()
 sc = spark.sparkContext
+# schema = [
+#     'order_id',
+#     'customer_id',
+#     'customer_name',
+#     'product_id',
+#     'product_name',
+#     'product_category',
+#     'payment_type',
+#     'qty',
+#     'price',
+#     'dateTime',
+#     'country',
+#     'city',
+#     'ecommerce_website',
+#     'payment_txn_id',
+#     'payment_txn_success',
+#     'failure_reason'
+# ]
+
+def main():
+    file = 'P2output.csv'
+    df = readData(file)
+    df.show()
+    df = cleanData(df)
+    df.show()
+    #df = cleanData(df)
+    pdDf = df.toPandas()
+    pdDf.to_csv("cleanedData.csv",encoding='utf-8', index=False)
+    
 
 def readData(file):
     df = spark.read.option("header", True).csv(file)
-    df.show()
+    return df
 
-file = 'P2output.csv'
-readData(file)
+def cleanData(df):
+    df = df.na.drop() #Remove rows with null values
+
+    #Filter out rows with invalid characters. [^a-zA-Z -] gets rows with invalid characters
+    #~ is a not function to exclude those rows
+    df = df.filter((~df.customer_name.rlike("[^a-zA-Z' -]")))
+    df = df.filter((~df.product_name.rlike("[^a-zA-Z' -]")))
+    df = df.filter((~df.product_category.rlike("[^a-zA-Z' -]")))
+    df = df.filter((~df.city.rlike("[^a-zA-Z' -]")))
+    return df
+
+main()
